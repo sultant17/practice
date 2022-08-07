c6582292fc3fe8d97368a0664689396998694b5b
Sultan Toiganbayev <stoiganbayev@Sultans-MacBook-Pro.local>
2022-08-07 12:23:55 +0600

    update

--- test.py
+++ test.py
@@ -2,8 +2,17 @@ def hello():
     print('Hello, World!')
 
 
+def test_add():
+    assert add(4, 5) == 9
+    assert add(-10, 10) == 0
+    assert add(4, 4) == 8
+
 def main():
     hello()
 
+def add(x, y):
+    return x + y + 1
+
 if __name__ == '__main__':
-    main()
+    test_add()
+
